096a4e86039d89e87c5c71dfb0be3c54559c1042
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2023-11-11 20:27:28 +0100

    js exercises 10 and 11

--- JavaScript/avanzado/Ej_10_desplazamientoCesar.js
+++ JavaScript/avanzado/Ej_10_desplazamientoCesar.js
@@ -1,13 +1,61 @@
 /*
 El cifrado César es una técnica de cifrado muy simple que se utiliza para ocultar texto al cambiar cada letra por una letra con un 
 desplazamiento fijo en el alfabeto. Crea una función en JavaScript que realice un cifrado César. Para ello:
-Crea una función llamada cifrarCesar que reciba dos argumentos: una cadena de texto y un número entero que representará el 
-desplazamiento en el cifrado.
-La función debe cifrar la cadena de texto moviendo cada letra en el alfabeto hacia adelante en la posición según el número de 
+
+1. ✅ Crea una función llamada cifrarCesar que reciba dos argumentos: una cadena de texto y un número entero que representará el 
+desplazamiento en el cifrado. 
+2. ✅ La función debe cifrar la cadena de texto moviendo cada letra en el alfabeto hacia adelante en la posición según el número de 
 desplazamiento. Por ejemplo, si el desplazamiento es 3, "A" se convierte en "D", "B" se convierte en "E", y así sucesivamente. 
 Asegúrate de mantener la misma capitalización (mayúsculas y minúsculas).
-La función debe ignorar los caracteres que no son letras y mantenerlos sin cambios.
-Si se llega al final del alfabeto, el desplazamiento debe continuar desde el principio. Por ejemplo, si el desplazamiento es 3 y 
+3. ✅ La función debe ignorar los caracteres que no son letras y mantenerlos sin cambios.
+4. ✅ Si se llega al final del alfabeto, el desplazamiento debe continuar desde el principio. Por ejemplo, si el desplazamiento es 3 y 
 la letra es "Z", se convierte en "C".
-La función debe devolver la cadena cifrada.
+5. ✅ La función debe devolver la cadena cifrada.
 */
+
+let desplazamiento = 3;
+let cadena = "hOlA 123345ñ6789 mUnDo ZyXw";
+
+function cifrarCesar(cadena, desplazamiento) 
+{
+    const abecedario = "abcdefghijklmnopqrstuvwxyz".split('');
+    let cadenaCifrada = "";
+    let caracter;
+    let caracterMinuscula;
+    let caracterCifrado;
+    let indiceAuxiliar;
+
+    for (let i = 0; i < cadena.length; i++) 
+    {
+        caracter = cadena.charAt(i);
+        caracterMinuscula = cadena.charAt(i).toLowerCase();
+
+        if(abecedario.includes(caracterMinuscula))
+        {
+            if(abecedario.indexOf(caracterMinuscula)+desplazamiento >= abecedario.length)
+            {
+                indiceAuxiliar =  abecedario.indexOf(caracterMinuscula) + desplazamiento - abecedario.length;
+                caracterCifrado = abecedario[indiceAuxiliar];
+            }
+            else
+            {
+                caracterCifrado = abecedario[abecedario.indexOf(caracterMinuscula)+desplazamiento];
+            }
+
+            if(caracter != caracterMinuscula)
+            {
+                caracterCifrado = caracterCifrado.toUpperCase();
+            }
+
+            cadenaCifrada += caracterCifrado;
+        }
+        else
+        {
+            cadenaCifrada += caracter;
+        }
+    }
+
+    return (cadenaCifrada);
+}
+
+console.log(cifrarCesar(cadena, desplazamiento));
---
+++ JavaScript/avanzado/Ej_11_formatoArchivo.js
@@ -0,0 +1,32 @@
+/*
+Crea un script que reciba el nombre de la imagen que desea adjuntar a una supuesta URL.
+El script debe:
+Comprobar si el nombre proporcionado incluye una extensión.
+Si la incluye, debe verificar si es válida para el sistema (considera que son válidas .png y .jpg). 
+Si es válida, debe mostrarle un mensaje diciendo ¡Sí se puede adjuntar!. En caso contrario, 
+debe mostrarle un mensaje diciendo que la extensión no es válida, y que debe adjuntar un archivo 
+llamado nombreImagen.png o nombreImagen.jpg, donde nombreImagen será el nombre de la imagen que 
+haya introducido el usuario.
+Comprueba por consola que funciona, probando estos argumentos: foto.png, foto.pdf y foto.jpg
+*/
+
+function comprobar_extension_imagen(nombreImagenCompleto) 
+{
+    const extensionesValidas = [".png", ".jpg"];
+    let extensionImagen = nombreImagenCompleto.slice(nombreImagenCompleto.indexOf("."));   
+
+    if(extensionesValidas.includes(extensionImagen))
+    {
+        console.log("¡Sí se puede adjuntar!.");
+    }
+    else
+    {   
+        let nombreImagen = nombreImagenCompleto.slice(0, nombreImagenCompleto.indexOf("."),nombreImagenCompleto);
+        console.log("La extensión no es valida. Debe adjuntar un archivo llamado " 
+        + nombreImagen + ".png" + " o " + nombreImagen + ".jpg.");
+    }
+}
+
+comprobar_extension_imagen("foto_1.png");
+comprobar_extension_imagen("foto_2.pdf");
+comprobar_extension_imagen("foto_3.jpg");
