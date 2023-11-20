6c4535cbdd4f54100815dc652d67bb2d929fb7da
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2023-11-20 18:01:57 +0100

    exercise 19

--- JavaScript/avanzado/Ej_19_cookieExplicacion.js
+++ JavaScript/avanzado/Ej_19_cookieExplicacion.js
@@ -8,16 +8,16 @@ tomorrow.setDate(tomorrow.getDate() + 1);
 var usuarioCookie = "usuario=ana; expires=" + tomorrow.toUTCString() + "; path=/; domain=ejemplo.com";
 var rolCookie = "rol=administrador; expires=" + tomorrow.toUTCString() + "; path=/; domain=ejemplo.com";
 
-// 
+// Esta intentando introducir las variables creadas anteriormente en el lugar del documento donde se almacenan las cookies
 document.cookie = usuarioCookie;
 document.cookie = rolCookie;
 
-// Esto imprime:
+// Esto imprime: una cadena vacía porque las dos anteriores líneas de código dan error
 console.log(document.cookie)
 
 // SEGUNDA PARTE
 
-//
+// Seguiría dando error
 var cookies = document.cookie.split(';');
 
 // Buscar la...
@@ -28,13 +28,13 @@ for (var i = 0; i < cookies.length; i++)
 	if (cookie.indexOf("usuario=") === 0) 
 	{
 
-		//
+		// Sigue dando error
 		var cookieParts = cookie.split('=');
 
-		//
+		// Sigue dando error
 		var usuarioValue = cookieParts[1];
 
-		//
+		// Sigue dando error
 		var expiresIndex = cookie.indexOf("expires=");
 		var expiresValue = expiresIndex !== -1 ? cookie.substring(expiresIndex + 8).trim() : "No hay fecha de expiración especificada";
 
@@ -42,7 +42,7 @@ for (var i = 0; i < cookies.length; i++)
 		console.log("Valor de la cookie usuario:", usuarioValue);
 		console.log("Fecha de expiración de la cookie usuario:", expiresValue);
 
-		//
+		// Sigue dando error
 		break;
 	}
 }
