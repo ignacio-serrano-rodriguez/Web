1e3d3a3f5f7fe8a9917eb53eabc13f5898da14dd
Ignacio Serrano Rodríguez <ignacioserranorodriguez@gmail.com>
2024-01-19 20:39:34 +0100

    ejercicio 6 terminado

--- JavaScript/unidad_4/ej_4_forEachMap.js
+++ JavaScript/unidad_4/ej_4_forEachMap.js
@@ -1,13 +1,11 @@
-let frutas = ["manzana", "uva", "cereza", "kiwi"];
-let enteros = [23,87,12,34];
-let cadena = '';
-
 // 4.1 FOREACH
+let frutas = ["manzana", "uva", "cereza", "kiwi"];
 // Se tiene que ver en terminal porque con QUOKKA no funciona
 frutas.forEach((element) => console.log(element));
 
 // 4.2 MATH (max, min, pow)
 // Es necesario usar el operador spread (...)
+let enteros = [23,87,12,34];
 console.log(Math.max(...enteros));
 console.log(Math.min(...enteros));
 
--- JavaScript/unidad_4/ej_6_funcionFlecha.js
+++ JavaScript/unidad_4/ej_6_funcionFlecha.js
@@ -1,15 +1,11 @@
-/*
-6.1
+/* 6.1
 Crea una función flecha que se llame cuadrado y calcule el cuadrado de un número pasado por parámetro. 
-Luego, llama a dicha función con el argumento 6.
-*/
+Luego, llama a dicha función con el argumento 6. */
 let cuadrado = x => x*x;
 console.log(cuadrado(6));
 
-/*
-6.2
-4.3 pero usando función flecha
-*/
+/* 6.2
+4.3 pero usando función flecha */
 let frutas = ["manzana", "uva", "cereza", "kiwi"];
 
 let longitudes = frutas.map(function (fruta) 
