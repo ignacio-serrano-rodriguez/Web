108df9a18a2b076823452bad82ec536572320720
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2024-01-25 14:23:25 +0100

    empezando ejercicio 9

---
+++ JavaScript/unidad_4/ej_9_tareas.js
@@ -0,0 +1,9 @@
+/*
+Estás desarrollando una aplicación de gestión de tareas pendientes. Necesitas implementar funcionalidad para que 
+los usuarios puedan agregar, modificar y eliminar tareas de su lista. Cada tarea tiene un identificador único, una
+descripción y un estado (pendiente o completada). Conforme vayas añadiendo tareas, se mostrarán en la lista junto
+a su ID y estado. Debes diseñar funciones para: buscar una tarea por su identificador, eliminar una tarea específica
+y mostrar un listado de todas las tareas de la lista. Además, las funciones deben ser eficientes y proporcionar una
+experiencia fácil de usar para los usuarios. Crea una página web de interfaz con diseño amigable que cumpla lo 
+anterior.
+*/
