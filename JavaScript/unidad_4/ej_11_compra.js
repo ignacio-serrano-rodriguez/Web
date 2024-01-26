320b282e62fd83aadfd3380f3f0594cb55328930
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2024-01-26 15:08:48 +0100

    ejercicio 11 - hacer funcionalidades

--- JavaScript/unidad_4/ej_11_compra.js
+++ JavaScript/unidad_4/ej_11_compra.js
@@ -1,10 +1,13 @@
 /*
-Estás trabajando en una aplicación de lista de compras en la que los usuarios pueden crear y combinar sus propias 
-listas con los nombres de los productos. Cada lista tiene un nombre y un conjunto de productos. Necesitas 
-implementar funciones que permitan copiar una lista existente en otra y para unir varias listas. La capacidad de 
-los usuarios para organizar y combinar fácilmente sus listas es esencial para la usabilidad de la aplicación.
-Crea una página web de interfaz con diseño amigable que cumpla lo anterior.
+Estás trabajando en una aplicación de lista de compras en la que los usuarios pueden crear y combinar sus 
+propias listas con los nombres de los productos. Cada lista tiene un nombre y un conjunto de productos. 
+Necesitas implementar funciones que permitan copiar una lista existente en otra y para unir varias listas.
+La capacidad de los usuarios para organizar y combinar fácilmente sus listas es esencial para la usabilidad 
+de la aplicación. Crea una página web de interfaz con diseño amigable que cumpla lo anterior.
 */
+
+document.getElementById("nombreProducto").disabled = true;
+document.getElementById("botonAniadir").disabled = true;
 
 var listas = [];
 
@@ -14,51 +17,84 @@ function crearLista()
 	document.getElementById("nombreLista").value = "";
 	let listaExistente = false;
 
+	if(nombreLista == '')
+		document.getElementById("consola").innerHTML = `No puedes introducir una cadena vacia.`;
+	else
+	{
+		for (let i = 0; i < listas.length; i++) 
+		{
+			if(nombreLista == listas[i][0])
+			{
+				listaExistente = true;
+				document.getElementById("consola").innerHTML = `La lista (${nombreLista}) ya existe.`;
+				break;
+			}
+		}
+
+		if(listaExistente == false)
+		{
+			listas.push([nombreLista]);
+			document.getElementById("consola").innerHTML = `Lista (${nombreLista}) creada.`;
+			document.getElementById("nombreProducto").disabled = false;
+			document.getElementById("botonAniadir").disabled = false;
+		}
+			
+		console.log(listas);
+		actualizacionListas();
+	}
+}
+
+
+function actualizacionListas() 
+{
+	let divListas = document.getElementById("listas");
+	let divListasProductos = document.getElementById("listasProductos");
+	divListas.innerHTML = "";
+	divListasProductos.innerHTML = "";
+	let select = document.createElement("select");
+	select.id = "selectProductos";
+
+	for (let i = 0; i < listas.length; i++) 
+	{
+		let p = document.createElement("p");
+		p.innerHTML = `${listas[i][0]}`;
+		divListas.append(p);
+
+		let option = document.createElement("option");
+		option.value = `${listas[i][0]}`
+		option.textContent = `${listas[i][0]}`;;
+		select.append(option);
+
+		let ul = document.createElement("ul");
+		for (let j = 1; j < listas[i].length; j++) 
+		{
+			let li = document.createElement("li");
+			li.innerHTML = `${listas[i][j]}`;
+			ul.append(li);
+		}
+
+		divListas.append(ul);		
+	}
+
+	divListasProductos.append(select);
+}
+
+function aniadirProducto() 
+{
+	let nombreProducto = document.getElementById("nombreProducto").value.trim();
+	let nombreLista = document.getElementById("selectProductos").value.trim();
+
 	for (let i = 0; i < listas.length; i++) 
 	{
 		if(nombreLista == listas[i][0])
 		{
-			listaExistente = true;
-			document.getElementById("consola").innerHTML = `La lista (${nombreLista}) ya existe.`;
+			console.log(listas[i]);
+			listas[i].push(nombreProducto);
 			break;
-		}
+		}	
 	}
 
-	if(listaExistente == false)
-	{
-		listas.push([nombreLista]);
-		document.getElementById("consola").innerHTML = `Lista (${nombreLista}) creada.`;
-	}
-		
-	console.log(listas);
+	nombreProducto = document.getElementById("nombreProducto").value = "";
+
 	actualizacionListas();
-}
-
-function actualizacionListas() 
-{
-	document.getElementById("listas").innerHTML="";
-	document.getElementById("productos").innerHTML=`<select id="lista">`;
-
-	for (let i = 0; i < listas.length; i++) 
-	{
-		// Productos
-		document.getElementById("productos").innerHTML+=`
-			<option value="${listas[i][0]}">${listas[i][0]}</option>
-		`;
-		
-		// Listas
-		document.getElementById("listas").innerHTML+=`
-			<p>${listas[i][0]}</p>
-			<ul>
-		`;
-			for (let j = 1; j < listas[i].length; j++) 
-			{
-				document.getElementById("listas").innerHTML+=`
-					<li>${listas[i][j]}</li>
-				`;
-			}
-		document.getElementById("listas").innerHTML+=`
-			<ul/>
-		`
-	}
 }
