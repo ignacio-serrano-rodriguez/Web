2d86e43a510a38206aa6546ebcc3561bd6d1be38
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2024-02-02 13:22:30 +0100

    ejercicio 1 terminado

--- PHP/doctrine/ej1.php
+++ PHP/doctrine/ej1.php
@@ -5,8 +5,54 @@ Si el equipo no existe mostrar un mensaje apropiado para el usuario.
 */
 require_once "bootstrap.php";
 require_once './src/Equipo.php';
-echo"
-	<input type='text' placeholder='Introduce un ID'/>
-	<input type='button' value='Obtener equipo'/>
+
+$idEquipo = $_POST["idEquipo"] ?? null;
+$existenciaEquipo = true;
+
+if($idEquipo == null)
+	$existenciaEquipo = false;
+else
+	$equipo = $entityManager->find("Equipo",$idEquipo);
+	if($equipo == null)
+		$existenciaEquipo = false;
+	else
+	{
+		$nombreEquipo = $equipo->getNombre();
+		$fundacionEquipo = $equipo->getFundacion();
+		$sociosEquipo = $equipo->getSocios();
+		$ciudadEquipo = $equipo->getCiudad();
+	}
+
+echo
+"
+	<form action='ej1.php' method=post>
+		<p id='consola'></p>
+		<input id='idEquipo' name='idEquipo' type='text' placeholder='Introduce ID equipo'/>
+		<input type='submit' value='Obtener su información'/>
+	</form>
 ";
 
+if($existenciaEquipo == false)
+{
+	echo 
+	"
+		<script>
+			document.getElementById('consola').innerHTML=`Introduce un ID de equipo válido.`;
+		</script>
+	";
+}
+else
+{
+	echo 
+	"
+		<script>
+			document.getElementById('consola').innerHTML=`	
+
+				Nombre ($nombreEquipo)<br/>
+				Fundación ($fundacionEquipo)<br/>
+				Socios ($sociosEquipo)<br/>
+				Ciudad ($ciudadEquipo)
+			`;
+		</script>
+	";
+}
