9df94d0b8847bd2f59744d6670c94c2ee1254a5c
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2024-02-12 12:27:28 +0100

    inicio madrid

---
+++ JavaScript/unidad_5_angular/madrid/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LugaresComponent } from './lugares/lugares.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LugaresComponent,
+    TarjetaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
