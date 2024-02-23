826d87e2eced1243e59207cc0c1e9aaf28c3338e
Ignacio Serrano Rodriguez <ignacioserranorodriguez@gmail.com>
2024-02-23 21:13:09 +0100

    3 proyectos terminados

--- PHP/proyecto_consumirCompaniero/llamarAPI_companiero.js
+++ PHP/proyecto_consumirCompaniero/llamarAPI_companiero.js
@@ -14,13 +14,13 @@ async function mostrar_productos()
 		document.getElementById("contenido").innerHTML += 
 		`
 			<strong>ID</strong> ${value.id_producto}<br/>
-			<strong>Nombre</strong> <input value="${value.nombre}"/><br/>
-			<strong>Precio</strong>  <input value="${value.precio}"/><br/>
-			<strong>Cantidad</strong> <input value="${value.cantidad}"/><br/>
-			<strong>Tipo</strong> <input value="${value.tipo}"/><br/>
-			<strong>Ventas</strong> <input value="${value.ventas}"/><br/><br/>
-			<input onclick="borrar_producto()" type="button" value="❌ Eliminar"/>
-			<input onclick="modificar_producto()" type="button" value="🔵 Modificar"/>
+			<strong>Nombre</strong> <input id="nombre${value.id_producto}" value="${value.nombre}"/><br/>
+			<strong>Precio</strong>  <input id="precio${value.id_producto}" value="${value.precio}"/><br/>
+			<strong>Cantidad</strong> <input id="cantidad${value.id_producto}" value="${value.cantidad}"/><br/>
+			<strong>Tipo</strong> <input id="tipo${value.id_producto}" value="${value.tipo}"/><br/>
+			<strong>Ventas</strong> <input id="ventas${value.id_producto}" value="${value.ventas}"/><br/><br/>
+			<input onclick="eliminar_producto(${value.id_producto})" type="button" value="❌ Eliminar"/>
+			<input onclick="modificar_producto(${value.id_producto})" type="button" value="🔵 Modificar"/>
 			<br/><br/><br/>
 		`;
 	}
@@ -29,6 +29,7 @@ async function mostrar_productos()
 async function buscar_producto() 
 { 
 	document.getElementById("consola").innerHTML='';
+	document.getElementById("consolaAniadir").innerHTML='';
 
 	let id = document.getElementById("idProductoBuscar").value.trim();
 	if(id == '')
@@ -54,13 +55,13 @@ async function buscar_producto()
 				document.getElementById("contenido").innerHTML = 
 				`
 					<strong>ID</strong> ${datos.id_producto}<br/>
-					<strong>Nombre</strong> <input value="${datos.nombre}"/><br/>
-					<strong>Precio</strong>  <input value="${datos.precio}"/><br/>
-					<strong>Cantidad</strong> <input value="${datos.cantidad}"/><br/>
-					<strong>Tipo</strong> <input value="${datos.tipo}"/><br/>
-					<strong>Ventas</strong> <input value="${datos.ventas}"/><br/><br/>
-					<input onclick="borrar_producto()" type="button" value="❌ Eliminar"/>
-					<input onclick="modificar_producto()" type="button" value="🔵 Modificar"/>
+					<strong>Nombre</strong> <input id="nombre${datos.id_producto}" value="${datos.nombre}"/><br/>
+					<strong>Precio</strong>  <input id="precio${datos.id_producto}" value="${datos.precio}"/><br/>
+					<strong>Cantidad</strong> <input id="cantidad${datos.id_producto}" value="${datos.cantidad}"/><br/>
+					<strong>Tipo</strong> <input id="tipo${datos.id_producto}" value="${datos.tipo}"/><br/>
+					<strong>Ventas</strong> <input id="ventas${datos.id_producto}" value="${datos.ventas}"/><br/><br/>
+					<input onclick="eliminar_producto(${datos.id_producto})" type="button" value="❌ Eliminar"/>
+					<input onclick="modificar_producto(${datos.id_producto})" type="button" value="🔵 Modificar"/>
 					<br/><br/><br/>
 				`;
 			}
@@ -70,16 +71,6 @@ async function buscar_producto()
 			document.getElementById("consola"). innerHTML = `Producto no encontrado.<br/><br/>`;
 		}
 	}
-}
-
-async function borrar_producto() 
-{ 
-	console.log("función borrar producto");
-}
-
-async function modificar_producto() 
-{ 
-	console.log("función modificar producto");
 }
 
 async function aniadir_producto() 
@@ -102,30 +93,89 @@ async function aniadir_producto()
 		parametros.append('cantidad', cantidad);
 		parametros.append('tipo', tipo);
 		parametros.append('ventas', ventas);
-
 		
-			const respuesta = await fetch(`${API}/productos`, 
+		const respuesta = await fetch(`${API}/productos`, 
+		{
+			method: 'POST', 
+			headers: 
 			{
-				method: 'POST', 
-				headers: 
-				{
 				'Content-Type': 'application/x-www-form-urlencoded'
-				},
-				body: parametros.toString() 
-			});
+			},
+			body: parametros.toString() 
+		});
 
-			if(respuesta.status != 200 && respuesta.status != 201)
-			{
-				document.getElementById("consolaAniadir").innerHTML = '<p>Todos los datos han de ser correctos.</p>';
-			}
-			else
-			{
-				document.getElementById("consolaAniadir").innerHTML = '';
-				document.getElementById("consolaAniadir").innerHTML = '<p>Producto creado y listado actualizado.</p>';
-				mostrar_productos();
-			}
+		if(respuesta.status != 200 && respuesta.status != 201)
+		{
+			document.getElementById("consolaAniadir").innerHTML = '<p>Todos los datos han de ser correctos.</p>';
+		}
+		else
+		{
+			document.getElementById("consolaAniadir").innerHTML = '<p>Producto creado y listado actualizado.</p>';
+			mostrar_productos();
+		}
 	}
 }
 
+async function eliminar_producto(id) 
+{ 
+	const respuesta = await fetch(`${API}/productos/${id}`, 
+	{
+		method: 'DELETE'
+	});
+
+	if(respuesta.status != 200 && respuesta.status != 201)
+	{
+		document.getElementById("consolaAniadir").innerHTML = '<p>No se ha eliminado ningún producto.</p>';
+	}
+	else
+	{
+		document.getElementById("consolaAniadir").innerHTML = '<p>Producto eliminado y listado actualizado.</p>';
+		mostrar_productos();
+	}
+}
+
+async function modificar_producto(id) 
+{ 
+	let nombre = document.getElementById(`nombre${id}`).value.trim();
+	let precio = document.getElementById(`precio${id}`).value.trim();
+	let cantidad = document.getElementById(`cantidad${id}`).value.trim();
+	let tipo = document.getElementById(`tipo${id}`).value.trim();
+	let ventas = document.getElementById(`ventas${id}`).value.trim();
+
+	if(nombre == '' || precio == '' || cantidad == '' || tipo == '')
+	{
+		document.getElementById("consola").innerHTML = '<p>Introduce todos los datos.</p>';
+	}
+	else
+	{
+		let parametros = new URLSearchParams();
+		parametros.append('nombre', nombre);
+		parametros.append('precio', precio);
+		parametros.append('cantidad', cantidad);
+		parametros.append('tipo', tipo);
+		parametros.append('ventas', ventas);
+		
+		const respuesta = await fetch(`${API}/productos/${id}`, 
+		{
+			method: 'PUT', 
+			headers: 
+			{
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: parametros.toString() 
+		});
+
+		if(respuesta.status != 200 && respuesta.status != 201)
+		{
+			document.getElementById("consolaAniadir").innerHTML = '<p>Todos los datos han de ser correctos.</p>';
+			mostrar_productos();
+		}
+		else
+		{
+			document.getElementById("consolaAniadir").innerHTML = '<p>Producto modificado y listado actualizado.</p>';
+			mostrar_productos();
+		}
+	}
+}
 
 mostrar_productos();
